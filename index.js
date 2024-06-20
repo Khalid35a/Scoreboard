cb6297b3199888ed341ec338528190624b065204
Khalid35a <kmasoudk0@gmail.com>
2024-06-20 17:28:18 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+homeScore = document.getElementById("homeScore");
+guestScore = document.getElementById("guestScore");
+
+
+function plusOneHome() {
+    homeScore.innerText++;
+}
+
+function plusTwoHome() {
+    homeScore.innerText++;
+    homeScore.innerText++;
+}
+
+function plusThreeHome() {
+    homeScore.innerText++;
+    homeScore.innerText++;
+    homeScore.innerText++;
+}
+
+function plusOneGuest() {
+    guestScore.innerText++;
+}
+
+function plusTwoGuest() {
+    guestScore.innerText++;
+    guestScore.innerText++;
+}
+
+function plusThreeGuest() {
+    guestScore.innerText++;
+    guestScore.innerText++;
+    guestScore.innerText++;
+}
